841837c5ab653ff8b78e7fc8f126fa7b7e98d057
Juan Huerta <teckuro6@gmail.com>
2025-08-23 14:52:44 -0400

    fix: forzar uso de nueva API URL en Vercel con variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-portafolio.up.railway.app/api',
+  apiUrl: process.env['API_URL'] || 'https://api-portafolio.up.railway.app/api',
   appUrl: 'https://angular-portafolio.vercel.app'
 };
